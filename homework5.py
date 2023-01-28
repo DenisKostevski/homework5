5d21405d5025d7fca375f98cef48addb833a10cd
adm1n <deniskostevski@gmail.com>
2023-01-28 12:47:23 +0200

    homework5 colorama

--- homework5.py
+++ homework5.py
@@ -1,1 +1,21 @@
-#налголовніша функція це змінення кольорів тексту
+import sys
+print(sys.executable)
+print(sys.version)
+print(sys.platform)
+
+print("Бібліотека Colorama."
+      "Одна з найпопулярніших бібліотек для редагування кольору тексту а також поліпшеної читабельності виводу."
+      "\n Version: 0.4.6 "
+      "\n Summary: Cross-platform colored terminal text."
+      "\n Author-email: Jonathan Hartley <tartlay@tartley.com>")
+
+from colorama import Fore, Back, Style
+print(Fore.MAGENTA + 'some magenta text')
+print(Back.LIGHTCYAN_EX + 'and with a cyan background')
+print(Style.DIM + 'and in a dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
+print("Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET."
+      "\nBack: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET."
+      "\nStyle: DIM, NORMAL, BRIGHT, RESET_ALL")
